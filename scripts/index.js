84e097c5aa00c4bd6fa7820cd0a3cadb8fa5bfcb
yasminhernandes <yasminhernandesjobs@gmail.com>
2022-03-09 10:43:44 -0300

    add all

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,19 @@
 // menu mobile
 const menuHamburguer = document.querySelector(".header__menu-hamburguer");
 const sidebar = document.querySelector(".sidebar");
+const mainRight = document.querySelector(".main__block--right");
+
 let showFullMenu = true;
 
 const menuHamburguerActive = () => {
-  document.body.style.overflow = showFullMenu ? "hidden" : "initial";
-  menuHamburguer.classList.toggle("on", showFullMenu);
-  showFullMenu
-    ? (sidebar.style.display = "initial")
-    : (sidebar.style.display = "none");
-  showFullMenu = !showFullMenu;
-};
+  if (window.innerWidth <= 768) {
+    document.body.style.overflowY = showFullMenu ? "hidden" : "initial";
+    mainRight.style.opacity = showFullMenu ? "0.7" : "1";
+    menuHamburguer.classList.toggle("on", showFullMenu);
+    sidebar.classList.toggle("sidebar--mobile", showFullMenu);
 
+    showFullMenu = !showFullMenu;
+  }
+};
 
 menuHamburguer.addEventListener("click", menuHamburguerActive);
