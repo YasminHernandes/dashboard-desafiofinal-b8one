76274a7e32600a5e0c807f137d9121e28ce8e551
yasminhernandes <yasminhernandesjobs@gmail.com>
2022-03-09 23:37:17 -0300

    add files

--- scripts/authenticate.js
+++ scripts/authenticate.js
@@ -1,37 +1,40 @@
-let inputEmail = document.querySelector(".container-block__input--email");
-let inputPassword = document.querySelector(".container-block__input--password");
+// let inputEmail = document.querySelector(".container-block__input--email");
+// let inputPassword = document.querySelector(".container-block__input--password");
 
-const formButton = document.getElementsByClassName("container-block__button");
-const formLogin = document.querySelector(".container-block__form");
+// const formButton = document.getElementsByClassName("container-block__button");
+// const formLogin = document.querySelector(".container-block__form");
 
-const errorMessage = `
-      <span class="container__error-message">
-        Este e-mail não existe, você tem outro?'
-      </span>
-      `;
+// const errorMessage = `
+//       <span class="container__error-message">
+//         Este e-mail não existe, você tem outro?'
+//       </span>
+//       `;
 
-const authenticate = async () => {
-  const AUTH__URL = await fetch("https://test-final.b8one.academy/login", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: "academy@b8one.com",
-      password: "Academy2022",
-    }),
-  });
-  const response = await AUTH__URL.json();
-  console.log(response);
+// const authenticate = async () => {
+//   const AUTH__URL = await fetch("https://test-final.b8one.academy/login", {
+//     method: "POST",
+//     headers: {
+//       Accept: "application/json",
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       email: "academy@b8one.com",
+//       password: "Academy2022",
+//     }),
+//   });
+//   const response = await AUTH__URL.json();
+//   console.log(response);
 
-//   // if (authenticate === !authenticate) {
-//   //   inputEmail.classList.add("error");
-//   //   inputEmail.insertAdjacentHTML("afterend", errorMessage);
-//   //   inputEmail.style.marginBottom = "8px";
-//   //   console.log("false: ");
-//   // } else {
-//   //   formLogin.submit;
-//   // }
+//   formButton.onsubmit = () => {
+//     if (inputEmail !== "") {
+//       if (authenticate === !authenticate) {
+//         inputEmail.classList.add("error");
+//         inputEmail.insertAdjacentHTML("afterend", errorMessage);
+//         inputEmail.style.marginBottom = "8px";
+//         console.log("false: ");
+//         response.redirect("/");
+//       }
+//       response.redirect("/pages/index.html");
+//     }
+//   };
 // };
-
